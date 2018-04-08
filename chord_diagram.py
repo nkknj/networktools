7772a55c1abc1df763cefe2a2114b9b3ae05136b
nkknj <p8580393@gmail.com>
2018-04-08 15:22:20 +0900

    node color gradation by the sum of links

--- chord_diagram.py
+++ chord_diagram.py
@@ -3,16 +3,23 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 pi=np.pi
 
 data=sys.argv[1]
-data=np.loadtxt(data, delimiter=',')
+data=np.loadtxt(data, delimiter=' ')
 n=data.shape[0]
 theta=np.arange(0, 2*pi, 2*pi/n)
 if n%2==0:
 	theta+=pi/n
 
-plt.plot(np.sin(theta), np.cos(theta), 'bo')
+data=(data+data.T)/2
+
+for i in range(n):
+	data[i, i]=0
+
+for i in range(n):
+	plt.plot(np.sin(theta[i]), np.cos(theta[i]), marker='o', color=cm.Blues(np.sum(data, axis=1)[i]/np.max(np.max(data))))
 
 maxlw=np.max(data)
 T=np.linspace(0, 1, 21)
@@ -24,9 +31,12 @@ while i<n-1:
 		p2=np.array([np.sin(theta[j]), np.cos(theta[j])])
 		x=p0[0]*(1-T)**2+p2[0]*T**2
 		y=p0[1]*(1-T)**2+p2[1]*T**2
-		plt.plot(x, y, '-', lw=5*data[i, j]/maxlw)
+		plt.plot(x, y, 'b-', lw=data[i, j]/maxlw)
 		j+=1
 	i+=1
+
+plt.xlim(-1.1, 1.1)
+plt.ylim(-1.1, 1.1)
 
 fig=plt.gcf()
 fig.gca().set_aspect('equal')
