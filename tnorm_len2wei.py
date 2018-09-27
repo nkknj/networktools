7fe70c16e6f24e3e2749818cdc3a5db495463fc5
nkknj <p8580393@gmail.com>
2018-09-27 12:50:06 +0900

    add tnorm_len2wei.py

---
+++ tnorm_len2wei.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+import networktools
+
+input_filename=sys.argv[1]
+output_filename=sys.argv[2]
+
+matrix=np.loadtxt(input_filename, delimiter=',')
+
+matrix=networktools.shortest_path_length(matrix)
+matrix=(matrix+1)**(-1)
+
+for i in range(matrix.shape[0]):
+	matrix[i, i]=0
+
+np.savetxt(output_filename, matrix, delimiter=',')
